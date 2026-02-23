796dc755d425b2aa75f44e5174dcd619126f17ec
Kukharenko Yurii <46027349+yurakukh@users.noreply.github.com>
2026-02-23 22:34:19 +0200

    Checkstyle fix

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,6 +1,6 @@
 package core.basesyntax.thread;
 
-public class MyThread extends Thread{
+public class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 20; i++) {
